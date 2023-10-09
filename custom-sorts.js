f09fc5bb0c186a88ab92b4bed2f2931cdcc7528c
zacharybaca <zachary.baca@msn.com>
2023-10-09 18:03:00 -0500

    added

--- custom-sorts.js
+++ custom-sorts.js
@@ -14,9 +14,33 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0 && arr[i - 1] % 2 === 0) {
+      [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+      console.log('Arr: ', arr);
+    }
+
+  }
+  arr.sort(function (a,b) {
+    
+  })
+  // let evenArray = [];
+  // let oddArray = [];
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] % 2 === 0) {
+  //     evenArray.push(arr[i])
+  //   } else {
+  //     oddArray.push(arr[i]);
+  //   }
+  // }
+
+  // let newArr = oddArray.sort((a, b) => a - b).concat(evenArray.sort((a, b) => a - b));
+  // return newArr;
 }
 
+console.log(oddEvenSort([5, 4, 7, 2, 9, 8, 1, 6, 3]))
 function validAnagrams(s, t) {
   // Your code here
 }
